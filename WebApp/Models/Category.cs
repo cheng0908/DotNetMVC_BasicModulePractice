7e17d8e7e31384a01994ec091deb999993f8f5de
cheng0908 <58242000+cheng0908@users.noreply.github.com>
2023-12-05 00:04:47 +0800

    Add project files.

---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public  int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(0, 100, ErrorMessage = "Display order must be between 0 and 100 only!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
